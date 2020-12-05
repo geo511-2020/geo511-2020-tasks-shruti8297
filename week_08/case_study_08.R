f3a8de4e92bd5f0becbf61dc569d8743c40e6728
shruti8297 <sjagini@buffalo.edu>
2020-12-05 15:08:19 -0500

    done

--- week_08/case_study_08.R
+++ week_08/case_study_08.R
@@ -1,26 +1,20 @@
-# CO2 Annual Mean Data
 
 library(tidyverse)
-annual_mean <- read_table("ftp://aftp.cmdl.noaa.gov/products/trends/co2/co2_annmean_mlo.txt", skip = 56)
-
-# Data Plotted
-
+annual <- read_table("ftp://aftp.cmdl.noaa.gov/products/trends/co2/co2_annmean_mlo.txt", skip = 56)
 
 library(ggplot2)
 install.packages("ggthemes")
 library(ggthemes)
-figure <- ggplot(annual_mean, aes(year, mean)) + geom_line() +
-  labs(x = "Year", y = "Annual Mean", title = "Annual Mean CO2 1958 - 2019") +
+plot <- ggplot(annual, aes(year, mean)) + geom_line() +
+  labs(x = "Year", y = "Annual", title = "Annual Mean Carbon dioxide 1958 - 2019") +
   theme_bw()
 
-figure
-
 library(dplyr)
-recent_decade <- annual_mean %>%
+
+new<- annual %>%
   arrange(desc(year)) %>% 
   top_n(n = 10)
 
-
 library(kableExtra)
-kable(head(slice(recent_decade, desc(year))), format = "simple",
-             align = "c", caption = 'Annual Means for the Last Decade')
+kable(head(slice(new, desc(year))), format = "simple",
+             align = "c", caption = 'Annual Means for new decade')
